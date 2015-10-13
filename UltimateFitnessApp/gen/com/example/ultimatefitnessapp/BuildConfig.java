a5c2ecccc751c0d0cd90885267a8af7584d3d98f
Syed Sufyan Sher <ssher@hawk.iit.edu>
2015-10-13 12:22:39 -0500

    first commit with work

---
+++ UltimateFitnessApp/gen/com/example/ultimatefitnessapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.ultimatefitnessapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ UltimateFitnessApp/src/com/example/ultimatefitnessapp/OptionActivity.java
@@ -0,0 +1,66 @@
+package com.example.ultimatefitnessapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class OptionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_option);
+		
+		Button bBMI = (Button) findViewById(R.id.bBMI);
+		bBMI.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				//this will have all the things that the button Compute will do on click
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					//go to aws option page
+					
+					Intent intent= new Intent(OptionActivity.this,BMIActivity.class);
+					
+					EditText heightField = (EditText) findViewById(R.id.eHeight);
+					String heightS = heightField;
+					int height = Integer.parseInt(heightS);
+					EditText weightField = (EditText) findViewById(R.id.eWeight);
+					String weightS = weightField.toString();
+					int weight = Integer.parseInt(weightS);
+					
+					double bmi = (weight/2.20462)/((height/100)*(height/100));
+					
+					double TransText = bmi;
+	            	intent.putExtra("text",TransText);
+					
+					startActivity(intent);
+				}
+			});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.option, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
